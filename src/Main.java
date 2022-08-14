f558b086a69494bf6becd5acd4f87c9c10d87248
Vladimir <vladimir.duker@gmail.com>
2022-08-14 21:54:17 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
 		System.out.println(String_Int.getCount1(testString));
 		System.out.println(String_Int.getCount2(testString));
 		System.out.println(String_Int.getCount3(testString));
+		System.out.println(String_Int.getCount1(testString));
 	}
 }
