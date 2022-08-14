e9194c461eeb7306d98c4f73e3a067dd15bf7dbd
Vladimir <vladimir.duker@gmail.com>
2022-08-14 18:52:23 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println(String_Int.getCount1("dfjwfkjkaaafksdjfusfsufuasjdf"));
-		System.out.println(String_Int.getCount2("dfjwfkjkaaafksdjfusfsufuasjdf"));
+		String TEST = "dfjwfkjkaaafksdjfusfsufuasjdf";
+
+		System.out.println(String_Int.getCount1(TEST));
+		System.out.println(String_Int.getCount2(TEST));
 	}
 }
